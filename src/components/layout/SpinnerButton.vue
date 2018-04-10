f769961aeec4600864dfbce816b2d29ba9dbe317
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-04-10 15:19:27 +0200

    :sparkles: Add spinner and spinner-button

---
+++ src/components/layout/SpinnerButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button
+    type="button"
+    class="btn">
+    <slot v-if="!isSpinnerVisible"></slot>
+    <Spinner v-if="isSpinnerVisible"></Spinner>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+import Spinner from './Spinner.vue';
+
+@Component({
+  components: {
+    Spinner,
+  },
+  props: ['isSpinning'],
+})
+export default class SpinnerButton extends Vue {
+
+  public isSpinning: boolean;
+
+  public get isSpinnerVisible(): boolean {
+    return !!this.isSpinning;
+  }
+
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
