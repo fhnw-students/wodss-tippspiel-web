f1a45a6e990de82277eeec11cfe255fa37ca09f1
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-04-12 17:45:57 +0200

    :sparkles: Add POST, PUT, PATH & DELETE methods to the rest-client

--- src/lib/rest-client.ts
+++ src/lib/rest-client.ts
@@ -6,7 +6,6 @@ export interface RestClientConfiguration {
 }
 
 export interface THeaders { [name: string]: string; }
-// export type THeaders = { [name: string]: string };
 export type TBody = Blob | BufferSource | FormData | URLSearchParams | string;
 export type TIdentifier = string | number;
 
@@ -55,7 +54,6 @@ class RestClient {
   }
 
   public withoutJsonHeaders(): RestClient {
-    // this.withHeaders(jsonHeaders);
     this.withJsonHeaders = false;
     return this;
   }
@@ -81,6 +79,10 @@ class RestClient {
     return this.resource;
   }
 
+  // -------------------------------------------------------------------------
+  // GET
+  // -------------------------------------------------------------------------
+
   public async findOne<T>(id: TIdentifier): Promise<T> {
     const response = await this.fetchGet<T>(`${this.resource}/${id}`);
     if (this.model) {
@@ -101,13 +103,88 @@ class RestClient {
     const finalRequestOption: RequestInit = Object.assign({
       headers: this.getHeaders(),
     }, requestOption);
-    let response: Response;
-    response = await fetch(this.getRequestURL(requestURL, query), finalRequestOption);
-    if (response.status !== 200 && this.withJsonHeaders) {
+    const response: Response = await fetch(this.getRequestURL(requestURL, query), finalRequestOption);
+    if (response.status === 200 && this.withJsonHeaders) {
       return response.json();
     }
     return response as any;
   }
+
+  // -------------------------------------------------------------------------
+  // POST
+  // -------------------------------------------------------------------------
+
+  public create<T>(body: TBody): Promise<T> {
+    return this.fetchPost<T>(`${this.resource}`, body);
+  }
+
+  public async fetchPost<T>(requestURL: string, body: TBody, query?: string, requestOption?: RequestInit): Promise<T> {
+    const finalRequestOption: RequestInit = Object.assign({
+      method: 'POST',
+      headers: this.getHeaders(),
+    }, { body }, requestOption);
+    const response: Response = await fetch(this.getRequestURL(requestURL, query), finalRequestOption);
+    if ((response.status === 200 || response.status === 201) && this.withJsonHeaders) {
+      return response.json();
+    }
+    return response as any;
+  }
+
+  // -------------------------------------------------------------------------
+  // PUT & PATCH
+  // -------------------------------------------------------------------------
+
+  public update<T>(id: TIdentifier, body: TBody): Promise<T> {
+    return this.fetchPut<T>(`${this.resource}/${id}`, body);
+  }
+
+  public async fetchPut<T>(requestURL: string, body: TBody, query?: string, requestOption?: RequestInit): Promise<T> {
+    const finalRequestOption: RequestInit = Object.assign({
+      method: 'PUT',
+      headers: this.getHeaders(),
+    }, { body }, requestOption);
+    const response: Response = await fetch(this.getRequestURL(requestURL, query), finalRequestOption);
+    if (response.status === 200 && this.withJsonHeaders) {
+      return response.json();
+    }
+    return response as any;
+  }
+
+  public async fetchPatch<T>(requestURL: string, body: TBody, query?: string, requestOption?: RequestInit): Promise<T> {
+    const finalRequestOption: RequestInit = Object.assign({
+      method: 'PATCH',
+      headers: this.getHeaders(),
+    }, { body }, requestOption);
+    const response: Response = await fetch(this.getRequestURL(requestURL, query), finalRequestOption);
+    if (response.status === 200 && this.withJsonHeaders) {
+      return response.json();
+    }
+    return response as any;
+  }
+
+  // -------------------------------------------------------------------------
+  // DELETE
+  // -------------------------------------------------------------------------
+
+  public delete<T>(id: TIdentifier): Promise<T> {
+    return this.fetchDelete<T>(`${this.resource}/${id}`);
+  }
+
+  public async fetchDelete<T>(requestURL: string, query?: string, requestOption?: RequestInit): Promise<T> {
+    const finalRequestOption: RequestInit = Object.assign({
+      method: 'DELETE',
+      headers: this.getHeaders(),
+    }, requestOption);
+    const response: Response = await fetch(this.getRequestURL(requestURL, query), finalRequestOption);
+    if ((response.status === 200) && this.withJsonHeaders) {
+      return response.json();
+    }
+    return response as any;
+  }
+
+  // -------------------------------------------------------------------------
+  // PRIVATE
+  // -------------------------------------------------------------------------
 
   private configureHeaders(headers: THeaders): void {
     this.headers = Object.assign(this.headers, headers);
