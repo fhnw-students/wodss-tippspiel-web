b2bcb9dae70b0d95930a49a658d2687e0354a976
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-04-15 19:02:32 +0200

    :sparkles: Add gravatar component

---
+++ src/components/layout/Gravatar.vue
@@ -0,0 +1,43 @@
+<template>
+  <img :src="url" :alt="alt" @load="onLoad" @error="onError" />
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import md5 from 'md5';
+
+@Component
+export default class Gravatar extends Vue {
+
+  @Prop()
+  public email: string;
+
+  @Prop()
+  public size: number;
+
+  public defaultImg = 'retro';
+  public rating = 'g';
+  public alt = 'Avatar';
+
+  public get url(): string {
+    const img = [
+      '//www.gravatar.com/avatar/',
+      md5(this.email.trim().toLowerCase()),
+      `?s=${this.size || 80}`,
+      `&d=${this.defaultImg}`,
+      `&r=${this.rating}`,
+    ];
+
+    return img.join('');
+  }
+
+  public onLoad(...args: any[]): void {
+    this.$emit('load', ...args);
+  }
+
+  public onError(...args: any[]): void {
+    this.$emit('error', ...args);
+  }
+
+}
+</script>
