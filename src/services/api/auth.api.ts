984091dea3a04b250518b73a7038b5e3425848ef
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-04-15 19:06:45 +0200

    :sparkles: Add auth api service

---
+++ src/services/api/auth.api.ts
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+
+import { setToken, removeToken } from '@/services/token.service';
+
+/**
+ * Logins a user with his credentials to get a valid access token
+ */
+export async function signIn(username: string, password: string): Promise<void> {
+  const token = btoa(`${username}:${password}`);
+  const response = await Vue.$fetchClient()
+    .withoutCredentials()
+    .withHeaders({
+      Authorization: `Basic ${token}`,
+    })
+    .fetchPost('/auth/login');
+
+  if (response.status === 200) {
+    const body = await response.json();
+    return setToken(body.token);
+  }
+
+  throw new Error('Could not login');
+}
+
+/**
+ * Signs the user off the server and removes the access token from the localstorage.
+ */
+export async function signOut(): Promise<void> {
+  await Vue.$fetchClient().fetchGet('/auth/logout');
+  return removeToken();
+}
