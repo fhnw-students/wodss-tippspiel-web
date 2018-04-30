7f9b5fcca694eaa22479fc641be2123d0ae1b59e
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-04-30 15:13:22 +0200

    :sparkles: Add event-bus

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,7 @@ import '@/plugins/vuex.plugin';
 import '@/plugins/axios.plugin';
 import '@/plugins/noty.plugin';
 import '@/plugins/vee-validate.plugin';
+import '@/plugins/vue-event-bus.plugin';
 import { i18n } from '@/plugins/i18n.plugin';
 import { router } from '@/plugins/vue-router.plugin';
 
