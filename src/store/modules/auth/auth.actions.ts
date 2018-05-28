2eb13c1437f0a69132c8f56d5df354498077a13e
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-05-28 14:17:49 +0200

    :bug: Fix sign out process

--- src/store/modules/auth/auth.actions.ts
+++ src/store/modules/auth/auth.actions.ts
@@ -37,11 +37,9 @@ export const actions: ActionTree<AuthState, AuthState> = {
       .catch((err: any) => commit(mutationTypes.SIGN_IN_USER_FAILED, err));
   },
   [actionTypes.SIGN_OUT_USER]({ commit, state }: ActionContext<AuthState, AuthState>, credentials: Credentials): void {
-    authApi.signOut().then(() => {
-      commit(mutationTypes.SIGN_OUT_USER_SUCCESS);
-      router.push({
-        name: 'auth.login',
-      });
+    commit(mutationTypes.SIGN_OUT_USER_SUCCESS);
+    router.push({
+      name: 'auth.login',
     });
   },
   [actionTypes.CHECK_AUTH]({ commit, state }: ActionContext<AuthState, AuthState>, credentials: Credentials): void {
